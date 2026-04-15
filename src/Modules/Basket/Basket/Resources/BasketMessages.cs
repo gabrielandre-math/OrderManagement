9d0e402bb4e2c1ba621a6d342cdca41fde74b3cb
Developer <developer@interview.com>
2026-04-15 17:31:06 -0300

    feat: add ResXFileCodeGenerator and Designer.cs for Basket, Orders and Shared .resx visibility

--- src/Modules/Basket/Basket/Resources/BasketMessages.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Basket.Resources;
-
-/// <summary>
-/// Marker class for IStringLocalizer<BasketMessages> to locate
-/// the BasketMessages.resx resource files.
-/// </summary>
-internal class BasketMessages
-{
-}
--- src/Modules/Orders/Orders/Resources/OrdersMessages.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Orders.Resources;
-
-/// <summary>
-/// Marker class for IStringLocalizer<OrdersMessages> to locate
-/// the OrdersMessages.resx resource files.
-/// </summary>
-public class OrdersMessages
-{
-}
