967dc0459504ca99ee8c196353b132e743377d4b
gabriel <gabrieldevpyc@gmail.com>
2026-04-20 18:38:06 -0300

    feat: add getbyid

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using Carter;
+using Catalog.Products.Features.GetProducts;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Shared.Extensions;
+using Shared.Pagination;
+
+namespace Catalog.Products.Features.GetProductById;
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/products/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+            return result.ToProblemResult();
+        }).WithName("GetProductsById")
+            .WithSummary("List by id products")
+            .WithDescription("Returns a item by id  products from the catalog.")
+            .Produces<PaginatedResult<ProductDto>>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest);;
+    }
+}
